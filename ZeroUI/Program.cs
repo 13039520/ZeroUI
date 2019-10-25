8863a3f93c7cbf4946510ce758d3b43554f6d2ae
13039520 <735753456@qq.com>
2019-10-25 11:55:13 +0800

    edit

--- ZeroUI/Program.cs
+++ ZeroUI/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
@@ -23,11 +20,13 @@ namespace ZeroUI
         public static IWebHostBuilder CreateWebHostBuilder(string[] args)
         {
             var host = new WebHostBuilder();
-            host.UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseWebRoot(System.IO.Path.Combine(Directory.GetCurrentDirectory(), @"zeroUI"))
-                .UseStartup<Startup>();
-            return host;
+            return host.UseKestrel(options=> {
+                //options.Listen(System.Net.IPAddress.Loopback, 5000);
+                options.ListenLocalhost(5000);
+            })
+            .UseContentRoot(Directory.GetCurrentDirectory())
+            .UseWebRoot(System.IO.Path.Combine(Directory.GetCurrentDirectory(), @"zeroUI"))
+            .UseStartup<Startup>();
         }
     }
 }
